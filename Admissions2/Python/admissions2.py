e4492032a4d4746d92619c429e0701e9ab8c6984
Joshua <joshua.megnauth54@gmail.com>
2020-07-28 10:58:36 -0400

    Cleaned up graphs and started on the first model.

--- Admissions2/Python/admissions2.py
+++ Admissions2/Python/admissions2.py
@@ -7,6 +7,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Dracula colors
+# https://draculatheme.com/
+# Obviously not following the theme yet.
+PINK = "#ff79c6"
+CYAN = "#8be9fd"
+GREEN = "#50fa7b"
+PURPLE = "#bd93f9"
+RED = "#ff5555"
+BACKGROUND = "#282a36"
+HIST_KWARGS = {"edgecolor": BACKGROUND, "linewidth": 2}
+
+
+def set_options():
+    sns.set_context("poster")
+
 
 def clean_admissions(path: str) -> pd.DataFrame:
     """Load and clean admissions data.
@@ -37,15 +52,18 @@ def clean_admissions(path: str) -> pd.DataFrame:
 
     return admissions
 
-def eda_tests_density(admissions, size=18):
-    fig, axes=plt.subplots(ncols=2)
+def eda_tests_plots(admissions, size=18):
+    fig, axes=plt.subplots(nrows=2, ncols=2)
 
     fig.suptitle("GRE & TOEFL density plots", fontsize=size, weight="bold")
 
-    sns.distplot(admissions.gre, hist=True, color="blue",
-                 axlabel="GRE", ax=axes[0])
-    sns.distplot(admissions.toefl, hist=True, color="red",
-                 axlabel="TOEFL", ax=axes[1])
+    sns.distplot(admissions.gre, norm_hist=True, color=CYAN,
+                 axlabel="GRE", hist_kws=HIST_KWARGS, ax=axes[0, 0])
+    sns.distplot(admissions.toefl, norm_hist=True, color=PINK,
+                 axlabel="TOEFL", hist_kws=HIST_KWARGS, ax=axes[0, 1])
+
+    sns.scatterplot("cgpa", "gre", "uni_ratings", data=admissions,
+                    ax=axes[1, 0])
 
     return (fig, axes)
 
