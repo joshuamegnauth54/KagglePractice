5acdf768af948ba77fe91a8e94f1db9c996d4962
Joshua <joshua.megnauth54@gmail.com>
2020-08-16 23:11:26 -0400

    I got the colors to work! I have to figure out what to do to tidy the types column.

--- Pokemon_viz/Python/pokemon_viz.py
+++ Pokemon_viz/Python/pokemon_viz.py
@@ -6,11 +6,30 @@ import seaborn as sns
 
 # Globals
 SEED = 314
+poketype_colors = {"Water": "#6890F0",
+                   "Fire": "#F08030",
+                   "Grass": "#78C850",
+                   "Electric": "#F8D030",
+                   "Ice": "#98D8D8",
+                   "Psychic": "#F85888",
+                   "Dragon": "#7038F8",
+                   "Dark": "#705848",
+                   "Fairy": "#EE99AC",
+                   "Normal": "#A8A878",
+                   "Fighting": "#C03028",
+                   "Flying": "#A890F0",
+                   "Poison": "#A040A0",
+                   "Ground": "#E0C068",
+                   "Rock": "#B8A038",
+                   "Bug": "#A8B820",
+                   "Ghost": "#705898",
+                   "Steel": "#B8B8D0",
+                   "Unknown": "#68A090"}
+
 
 def cleaned_pokemon(path) -> pd.DataFrame:
     pokemon = pd.read_csv(path)
     abilities_temp_name = "abilities_tmp"
-    drop_temp_name = "to_drop"
 
     # Classification was totally not my misspelling
     pokemon.rename(columns={"classfication": "category",
@@ -20,7 +39,7 @@ def cleaned_pokemon(path) -> pd.DataFrame:
     # Melt types1/2 into a tidy column
     pokemon = pokemon.melt(id_vars=pokemon.columns.difference(["type1",
                                                                "type2"]),
-                           var_name=drop_temp_name,
+                           var_name="type_order",
                            value_name="type")
 
     # Clean up array specifiers in each ability list
@@ -39,13 +58,13 @@ def cleaned_pokemon(path) -> pd.DataFrame:
     # Melting produces a new DataFrame with duplicated entries for the
     # Pokémon with more than one ability.
     pokemon = pokemon.melt(id_vars=poke_orig_cols,
-                           var_name=drop_temp_name,
+                           var_name="type_order",
                            value_name="abilities")
 
     # Finally, drop the old columns as well as nulls introduced
     # from the split. Check out the R port of the code for an explanation.
-    cols_to_drop = [abilities_temp_name, drop_temp_name]
-    pokemon.drop(columns=cols_to_drop, inplace=True)
+    # cols_to_drop = [abilities_temp_name, drop_temp_name]
+    pokemon.drop(columns=abilities_temp_name, inplace=True)
     pokemon.dropna(subset=["abilities", "type"], inplace=True)
 
     # What's next? Let's fix up variable types!
